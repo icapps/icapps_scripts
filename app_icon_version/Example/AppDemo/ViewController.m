fc2fc95b2d82bf7b39a0a3dbe5d422cf0e141a44
Dylan Gyesbreghs <dylan.gyesbreghs@icapps.com>
2015-11-12 14:19:23 +0100

    Add example

---
+++ app_icon_version/Example/AppDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AppDemo
+//
+//  Created by Dylan Gyesbreghs on 12/11/15.
+//  Copyright © 2015 iCapps. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
